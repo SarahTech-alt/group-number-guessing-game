8302002e32f91fc64705454ebd194f385761109b
Sarah Fuoss <sfuoss@gmail.com>
2021-09-02 21:35:05 -0500

    created post function on server side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,7 +9,21 @@ function getGuesses(){
   const playerGuesses = {
     playerOneGuess: $('#player-one-guess').val(),
     playerTwoGuess: $('player-two-guess').val(),
-    playerThreeGuess: $('player-three-guess')
-  }
+    playerThreeGuess: $('player-three-guess').val(),
+  };
+  $.ajax({
+    method: 'POST',
+    url: '/guesses',
+    data: playerGuesses,
+  }).then(displayGuesses).catch(errorPostingGuesses)
+}
+
+function displayGuesses(response) {
+  console.log('Displaying Guesses');
+  console.log(response);
+};
+
+function errorPostingGuesses(response) {
+  console.log('Something broke!');
   
 }
--- server/server.js
+++ server/server.js
@@ -3,17 +3,28 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
-function randomNumber(min, max) {
+function randomNumber( min, max ) {
   return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}))
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+
+app.post('/guesses', (req,res) => {
+  console.log('in PST /guesses');
+  const numberToGuess = 5;
+  if (playerOneGuess == numberToGuess){
+    res.send('Player One Won!');
+  }
+  else {
+    res.send('Not a winner');
+  }
+})
 
 
 app.listen(PORT, () => {
